fbfb5e0c333b7e41b887f789d3756135413883c3
Confidence Madiope <confidencem97@gmail.com>
2022-06-29 10:16:28 +0200

    ADD: state, reducer and context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import React, { useReducer, createContext } from "react";
 
+function todosReducer(state, action) {
+  switch (action.type) {
+    default:
+      return todosInitialState;
+  }
+}
 
+export const TodosContext = createContext();
+
+const todosInitialState = {
+  todos: [
+    { id: 1, text: "finish writing code" },
+    { id: 2, text: "play a video game" },
+    { id: 3, text: "read a novel" },
+  ],
+};
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <div className='App'></div>;
 }
 
 export default App;
