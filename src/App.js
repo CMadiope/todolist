01dfebda07dca2d6df7069328cc8ee578e8ccefb
Confidence Madiope <confidencem97@gmail.com>
2022-06-29 12:36:31 +0200

    fix: edit feature

--- src/App.js
+++ src/App.js
@@ -9,12 +9,22 @@ function todosReducer(state, action) {
       const newToDo = { id: uuidv4(), text: action.payload };
       const addedToDos = [...state.todos, newToDo];
       return { ...state, todos: addedToDos };
-
     case "delete":
       const filteredTodoState = state.todos.filter(
         (todo) => todo.id !== action.payload.id
       );
       return { ...state, todos: filteredTodoState };
+    case "edit":
+      const updatedToDo = { ...action.payload };
+      const updatedToDoIndex = state.todos.findIndex(
+        (t) => t.id === action.payload.id
+      );
+      const updatedToDos = [
+        ...state.todos.slice(0, updatedToDoIndex),
+        updatedToDo,
+        ...state.todos.slice(updatedToDoIndex + 1),
+      ];
+      return { ...state, todos: updatedToDos };
     default:
       return todosInitialState;
   }
