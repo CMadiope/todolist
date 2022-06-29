6d8ac56575168540e3799d7a7a70edde10c0b979
Confidence Madiope <confidencem97@gmail.com>
2022-06-29 17:35:34 +0200

    ADD: useAPI custom hook to call mock API

---
+++ src/components/useAPI.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+import axios from 'axios'
+
+const useAPI = endpoint => {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    getData()
+  },[])
+
+  const getData = async() => {
+    const response = await axios.get(endpoint)
+    setData(response.data)
+  }
+}
