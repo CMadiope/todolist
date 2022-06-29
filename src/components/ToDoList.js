4490167e553463566a657bbf1c4ce25105cdf292
Confidence Madiope <confidencem97@gmail.com>
2022-06-29 11:45:12 +0200

    ADD: form for user to add todo

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,34 +1,44 @@
-import React,{useContext} from 'react'
-import { TodosContext } from '../App'
-import Table from 'react-bootstrap/Table'
-
+import React, { useContext, useState } from "react";
+import { TodosContext } from "../App";
+import { Table, Form, Button } from "react-bootstrap";
 
 const ToDoList = () => {
-  const {state, dispatch} = useContext(TodosContext)
+  const { state, dispatch } = useContext(TodosContext);
+  const [todoText, setTodoText] = useState("");
+
   return (
     <div>
+      <Form>
+        <Form.Group controlled='formBasicEmail'>
+          <Form.Control
+            type='text'
+            placeholder='Enter To Do'
+            onChange={(event) => setTodoText(event.target.value)}
+          />
+        </Form.Group>
+      </Form>
       <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>To Do</th>
-          <th>Edit</th>
-          <th>Delete</th>
-        </tr>
-      </thead>
-      <tbody>
-      {state.todos.map(todo => (
-        <tr key={todo.id}>
-          <td>{todo.text}</td>
-          <td>Edit</td>
-          <td onClick={() => dispatch({type:'delete',payload:todo})}>Delete</td>
-          
-        </tr>
-      ))}
-      </tbody>
+        <thead>
+          <tr>
+            <th>To Do</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {state.todos.map((todo) => (
+            <tr key={todo.id}>
+              <td>{todo.text}</td>
+              <td>Edit</td>
+              <td onClick={() => dispatch({ type: "delete", payload: todo })}>
+                Delete
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </Table>
     </div>
   );
-}
+};
 
-export default ToDoList
-
+export default ToDoList;
